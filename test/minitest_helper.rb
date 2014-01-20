fc4b9bc9628aea1ea72d51269359f0cea98a1043
cclaiborne <claiborne206@gmail.com>
2014-01-19 23:05:34 -0800

    Rails Resources Assignment

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,7 +2,6 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require "minitest/autorun"
 require "capybara/rails"
-require "active_support/testing/setup_and_teardown"
 
 class IntegrationTest < MiniTest::Spec
   include Rails.application.routes.url_helpers
