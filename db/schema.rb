9dbbff4ec1f3497dc34969b60fc0b6758de5dbfd
cclaiborne <claiborne206@gmail.com>
2014-01-29 17:25:12 -0800

    made changes to reflect user authorization

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140129205936) do
+ActiveRecord::Schema.define(version: 20140130002017) do
 
   create_table "posts", force: true do |t|
     t.text     "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "published"
   end
 
   create_table "projects", force: true do |t|
@@ -41,6 +42,7 @@ ActiveRecord::Schema.define(version: 20140129205936) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "author"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
